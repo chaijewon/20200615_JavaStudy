0e28128c09e2303b6ec3217243ef0f3b6fb35de3
vcand <vcand@DESKTOP-1DV7OA9>
2020-06-19 13:59:39 +0900

    자바기본

---
+++ 20200615-자바기본/src/MainClass.java
@@ -0,0 +1,25 @@
+/*
+ *   주석 처리 => 프로그램을 실행하지 않는다 
+ *   여러줄 주석 
+ */
+// 한줄 주석 
+/*
+ *   CTRL+SHIFT+/ => 주석
+ *   CTRL+SHIFT+\ => 주석해제 
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 도스창에 출력할 때 사용 
+		
+		  System.out.println("Hello Java"); 
+		  System.out.println("Hello Java");
+		  System.out.println("Hello Java"); 
+		  System.out.println("Hello Java");
+		  System.out.println("Hello Java");
+		 
+		
+	}
+
+}
---
+++ 20200615-자바기본/src/MainClass1.java
@@ -0,0 +1,64 @@
+
+public class MainClass1 {
+	static  public void main(String[] a) {
+	    // 표현 => 자바 
+	    /*
+	     *    정수 
+	     *      = 10진법   10,20...
+	     *      = 8진법
+	     *           011  => 9
+	     *           =
+	     *      = 16진법
+	     *           0x10
+	     *           ==
+	     *      = 2진법
+	     *           0b1010
+	     *           == 
+	     *    실수 
+	     *      = 저장 크기  => 4byte   10.5f
+	     *      = 저장 크기  => 8byte   10.5
+	     *    문자
+	     *      = ''  => 한글자만 저장 
+	     *    문자열 
+	     *      = ""  => 여러글자 저장 
+	     *      
+	     *      ASC (c) =======> 1byte
+	     *      Unicode (자바) => 2byte
+	     *      
+	     *      UTF-8
+	     */
+	    System.out.println('홍');
+	    System.out.println("Hello");
+	    System.out.println(10.5);
+	    System.out.println(10.5f);
+	    System.out.println(10.5F);
+	    
+	    System.out.println(10);// 명령프롬프트에 10을 출력 
+	    System.out.println(0b01010);
+	    System.out.println(012);
+	    System.out.println(0xA);
+	    /*
+	     *    1. 기본 문법 
+	     *    ===========
+	     *      => 대소문자 구분 
+	     *         => 라이브러리 (클래스:대소문자 , 메소드는 소문자 , 상수는 모두 대문자)
+	     *         PI=3.14
+	     *         
+	     *         변수/상수 
+	     *         
+	     *         int kor=100;
+	     *         final int KOR=200;
+	     *      => 명령문이 종료가 되면 반드시 ;
+	     *      
+	     *         
+	     *     3. 들여쓰기
+	     */
+     }
+}
+
+
+
+
+
+
+
