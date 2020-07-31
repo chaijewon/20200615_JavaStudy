536c61d467056d851466532291ab96760ccdde8b
vcand <vcand@DESKTOP-1DV7OA9>
2020-07-31 16:06:31 +0900

    1

---
+++ 20200731-자바마지막예제/src/com/sist/common/Function.java
@@ -0,0 +1,22 @@
+package com.sist.common;
+// Client , Server => 공통으로 사용되는 클래스
+// 프로그램의 기능 설정 => 내부 프로토콜 (서버와 클라이언트의 약속)
+// 웹 서버 => 
+public class Function {
+    // 로그인 처리 => 이미 로그인 된 사람 
+	public static final int LOGIN=100;
+	public static final int MYLOG=110;
+	
+	// 방만들기 
+	// 방들어가기 
+	// 방나가기 
+	// 대기실 수정 
+	
+	// 대기실 채팅
+	public static final int WAITCHAT=500;
+	// 방채팅
+	
+	// 쪽지보내기 
+	// 채팅 종료 
+	// 1:1 게임 
+}
